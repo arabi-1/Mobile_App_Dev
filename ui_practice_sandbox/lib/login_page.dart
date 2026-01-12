040fd4c1243aad4080255349d910e26a4a8ffde0
arabi-1 <khanarabi80@gmail.com>
2026-01-12 20:06:42 +0500

    Added all the buttons to the login screen

--- ui_practice_sandbox/lib/login_page.dart
+++ ui_practice_sandbox/lib/login_page.dart
@@ -48,28 +48,118 @@ class LoginScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: 10.0),
-            OutlinedButton(
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(minimumSize: Size(350, 50)),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Text(
-                    'Login in with Email',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(minimumSize: Size(350, 50)),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Text(
+                      'Login in with Email',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  Align(
-                    alignment: AlignmentGeometry.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsetsGeometry.only(right: 1),
-                      child: Icon(Icons.mail, color: Colors.white),
+                    Align(
+                      alignment: AlignmentGeometry.centerLeft,
+                      child: Padding(
+                        padding: EdgeInsetsGeometry.only(right: 1),
+                        child: Icon(Icons.mail, color: Colors.white),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(minimumSize: Size(350, 50)),
+                child: Stack(
+                  alignment: AlignmentGeometry.center,
+                  children: [
+                    Text(
+                      "Login with Google",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Align(
+                      alignment: AlignmentGeometry.centerLeft,
+                      child: Padding(
+                        padding: EdgeInsetsGeometry.only(right: 1),
+                        child: Icon(
+                          Icons.g_mobiledata_outlined,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(minimumSize: Size(350, 50)),
+                child: Stack(
+                  alignment: AlignmentGeometry.center,
+                  children: [
+                    Text(
+                      "Login with Apple",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Align(
+                      alignment: AlignmentGeometry.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsetsGeometry.only(right: 1),
+                        child: Icon(Icons.apple, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(minimumSize: Size(350, 50)),
+                child: Stack(
+                  alignment: AlignmentGeometry.center,
+                  children: [
+                    Text(
+                      'Login with Facebook',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Align(
+                      alignment: AlignmentGeometry.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 1),
+                        child: Icon(Icons.facebook, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
