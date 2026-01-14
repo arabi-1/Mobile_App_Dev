37d0748d1a099bdce4c694f657316fc7060aeef4
arabi-1 <khanarabi80@gmail.com>
2026-01-14 23:34:33 +0500

    Pushed the code of revision of ui

---
+++ ui_revision_homepage/lib/home_ui.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeUi extends StatelessWidget {
+  const HomeUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(backgroundColor: Colors.brown, appBar: AppBar(
+      title: Text("Practice Ui", style: TextStyle(color: Colors.white24),),
+      backgroundColor: ,),
+    );
+  }
+}
