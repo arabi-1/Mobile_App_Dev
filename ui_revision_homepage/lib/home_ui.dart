737e3e3c88e40a9b9ba083bc46f0191c0fe55d12
arabi-1 <khanarabi80@gmail.com>
2026-01-15 23:38:26 +0500

    Added one button and lines to the login page

--- ui_revision_homepage/lib/home_ui.dart
+++ ui_revision_homepage/lib/home_ui.dart
@@ -5,9 +5,67 @@ class HomeUi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(backgroundColor: Colors.brown, appBar: AppBar(
-      title: Text("Practice Ui", style: TextStyle(color: Colors.white24),),
-      backgroundColor: ,),
+    return Scaffold(
+      backgroundColor: Colors.brown,
+      appBar: AppBar(
+        title: Text("Practice Ui", style: TextStyle(color: Colors.white24)),
+        backgroundColor: Colors.amber,
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: CircleAvatar(
+              backgroundColor: Colors.grey,
+              child: Icon(Icons.person),
+            ),
+          ),
+          SizedBox(height: 8.0),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(minimumSize: Size(350, 50)),
+            onPressed: () {},
+            child: Stack(
+              alignment: AlignmentGeometry.center,
+              children: [
+                Text(
+                  "Login with Password",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentGeometry.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.password, color: Colors.green),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(child: Divider(color: Colors.black, thickness: 1)),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "ORR",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                Expanded(child: Divider(color: Colors.black, thickness: 1)),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
