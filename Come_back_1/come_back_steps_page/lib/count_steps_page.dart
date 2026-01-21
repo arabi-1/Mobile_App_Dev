18ec786ec4958da4a572974c72b0bc7b6f4dd02d
Muhammad Arabi <sp24-bse-031@isbstudent.comsats.edu.pk>
2026-01-21 20:28:14 +0500

    Completed the graph, added the cards and the single vertical graph

--- Come_back_1/come_back_steps_page/lib/count_steps_page.dart
+++ Come_back_1/come_back_steps_page/lib/count_steps_page.dart
@@ -68,47 +68,284 @@ class CountStepsPage extends StatelessWidget {
                   ),
                 ),
               ),
-
-              SizedBox(
-                height: 250,
-                width: 250,
-                child: Stack(
-                  children: [
-                    PieChart(
-                      PieChartData(
-                        centerSpaceRadius: 100,
-                        startDegreeOffset: 270,
-                        sections: [
-                          PieChartSectionData(
-                            color: Colors.pink,
-                            value: 25,
-                            showTitle: false,
-                          ),
-                          PieChartSectionData(
-                            color: Colors.indigo,
-                            value: 60,
-                            showTitle: false,
-                          ),
-                          PieChartSectionData(
-                            color: Colors.orange,
-                            value: 15,
-                            showTitle: false,
-                          ),
-                        ],
-                      ),
-                    ),
-                    Center(
-                      child: Column(
-                        children: [Text(provider.steps.toString())],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
             ],
           ),
         ),
         foregroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 15),
+            SizedBox(
+              height: 250,
+              width: 250,
+              child: Stack(
+                children: [
+                  PieChart(
+                    PieChartData(
+                      centerSpaceRadius: 70,
+                      startDegreeOffset: 270,
+                      sectionsSpace: 3,
+                      sections: [
+                        PieChartSectionData(
+                          radius: 20,
+                          color: Color(0xFFEE538D),
+                          value: 25,
+                          showTitle: false,
+                        ),
+                        PieChartSectionData(
+                          radius: 20,
+                          color: Color(0xFF394398),
+                          value: 60,
+                          showTitle: false,
+                        ),
+                        PieChartSectionData(
+                          radius: 20,
+                          color: Colors.orange,
+                          value: 15,
+                          showTitle: false,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          provider.steps.toString(),
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      '6000',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      'goal',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      '1.2',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      'km',
+                      style: TextStyle(
+                        color: Colors.deepOrangeAccent,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      '340',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      'kcal',
+                      style: TextStyle(
+                        color: Colors.yellowAccent,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.indigo,
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8, top: 8),
+                            child: Text(
+                              "246",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Text(
+                              '1893 ml',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          const Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Row(
+                              children: [
+                                Text(
+                                  "Drink water",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const SizedBox(width: 2),
+                                Icon(
+                                  Icons.add_outlined,
+                                  color: Colors.white,
+                                  size: 20,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.lightGreen,
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8, top: 8),
+                            child: Text(
+                              "246",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Text(
+                              '1893 ml',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          const Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Row(
+                              children: [
+                                Text(
+                                  "Drink water",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const SizedBox(width: 5),
+                                Icon(
+                                  Icons.add_outlined,
+                                  color: Colors.white,
+                                  size: 20,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                SizedBox(height: 10),
+                Column(
+                  children: [
+                    Container(
+                      height: 120,
+                      width: 14,
+                      child: Stack(
+                        alignment: AlignmentGeometry.bottomCenter,
+                        children: [
+                          Container(
+                            height: 120,
+                            width: 14,
+                            color: Colors.white24,
+                          ),
+                          FractionallySizedBox(
+                            heightFactor: 0.5,
+                            child: Container(color: Colors.indigo),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5),
+                Text(
+                  "January",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
