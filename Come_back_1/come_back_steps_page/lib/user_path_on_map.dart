449c1ce76982f9a1d485c2f0c52b5a5fefe25f4b
Muhammad Arabi <sp24-bse-031@isbstudent.comsats.edu.pk>
2026-01-29 23:37:15 +0500

    Boiler plate code for map on comeback app

---
+++ Come_back_1/come_back_steps_page/lib/user_path_on_map.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class UserPathOnMap extends StatelessWidget {
+  const UserPathOnMap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Map",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+      ),
+      body: Column(
+        
+      ),
+    );
+  }
+}
